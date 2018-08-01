e31a072543e27b5ddc35b9c3ec945980653aa4f3
James Verch <verchjames@gmail.com>
2018-08-01 14:44:34 -0400

    Implemented both queue.js and stack.js, passing tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,16 +4,31 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var counter = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    if (counter !== 0) {
+      counter--;
+      var returnVal = storage[0];
+      for (var key in storage) {
+        storage[key - 1] = storage[key];
+        delete storage[key];
+      }
+      return returnVal;
+    }
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
+  someInstance.items = storage;
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,15 +4,27 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var counter = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function() {
+    if (counter !== 0) {
+      counter--;
+      returnVal = storage[counter];
+      delete storage[counter];
+      return returnVal;
+    }
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
+  someInstance.items = storage;
   return someInstance;
 };
